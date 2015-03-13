bc723f54eb46d4ed029e2ead9792a55ab524505c
Alexander Martin <martina52@mymacewan.ca>
2015-03-13 14:17:09 -0400

    Finished most of CSS and master layout.

---
+++ laravel/app/views/master.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @if(isset($title))
+        <title>KIT-MTS | {{{ title }}}</title>
+    @else
+        <title>KIT-MTS</title>    
+    @endif
+    <meta charset="UTF-8">
+    {{ HTML::style("css/master.css") }}
+</head>
+<body>
+      <div id ="user">
+                @if(Auth::check()) 
+                    Welcome back, {{{ Auth::user()->Username }}} | <a href="#">Logout</a>
+                @else
+                    You are not logged in.
+                @endif
+        </div>
+
+    <div id="container">
+        <div id="banner">
+            <span id="logo">
+                EPL
+            </span>
+            <ul id="menu">
+                <a href="#" id="menuItem1" ><li>Book a kit</li></a>
+                <a href="#" id="menuItem2" ><li>View kits</li></a>
+                <a href="#" id="menuItem3" ><li>Summary</li></a>
+                <a href="#" id="menuItem4" ><li>Help</li></a>
+                <a href="#" id="menuItem5" ><li>Account</li></a>
+            </ul>
+        </div>
+     
+            <br style="clear:both;">
+
+            @if(Session::has('errors'))
+
+            @endif
+
+            @if(Session::has('message'))
+
+            @endif
+
+        
+
+        <div id="side">
+            <h1>Summary</h1>
+            <div id="content">
+                Sidebar stuff here!
+            </div>
+        </div>
+
+        <div id="main">
+            <h1>Page Title</h1>
+            <div id="content">
+                @yield('content')
+            </div>
+        </div>    <br style="clear:both;">
+        </div>
+    </div>
+</body>
+
+</html>
