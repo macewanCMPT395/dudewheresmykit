5e763784ba39db2c698cc9f21d169e232f07d768
magaw <magaw@mymacewan.ca>
2015-03-17 14:08:52 -0600

    Database Seeding
    Base database seeding;
    Values that can be obtained statically (such as blackouts and branch IDS) are obtained statically.
    All other tables are seeded with random values

--- laravel/app/database/seeds/DatabaseSeeder.php
+++ laravel/app/database/seeds/DatabaseSeeder.php
@@ -1,17 +1,22 @@
 <?php
-
 class DatabaseSeeder extends Seeder {
-
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
-
-		// $this->call('UserTableSeeder');
+	//Grabs the content of the XML tag $needle from the XML information $haystack
+	public static function grabTag($haystack, $needle){
+		$index = strpos($haystack, "<$needle>") + strlen($needle) + 2;
+		return substr($haystack, $index, strpos($haystack, "</$needle>", $index) - $index);
 	}
 
+	public function run() {
+		Eloquent::unguard();
+		//Standard
+		$this->call('BlackoutTableSeeder');
+		$this->call('BranchTableSeeder');
+		$this->call('StatusTableSeeder');
+
+		//Random
+		$this->call('UserTableSeeder');
+		$this->call('ItemTableSeeder');
+		$this->call('KitTableSeeder');
+		$this->call('BookingTableSeeder');
+	}
 }
