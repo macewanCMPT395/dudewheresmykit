89d1f929169d08ffad7f6d90a4f255e771cf0209
brookwellreuber <chaoticpsychosisss@gmail.com>
2015-03-18 16:12:20 -0400

    Update Email.php with fake email to test mailer

--- laravel/app/config/mail.php
+++ laravel/app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smpt.gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'Chaoticpsychosss@gmail.com', 'name' => 'David'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'randomtestmailer@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => ','P2ssword1234'
 
 	/*
 	|--------------------------------------------------------------------------
