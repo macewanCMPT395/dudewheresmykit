ed5060391d90ec7ab7b6beab5e690f8d13f896b5
brookwellreuber <chaoticpsychosisss@gmail.com>
2015-03-26 15:21:37 -0400

    Booking Backend Logic
    Backend connects to DB, Mailing is still a work in progress

--- laravel/app/models/Booking.php
+++ laravel/app/models/Booking.php
@@ -2,9 +2,9 @@
 
 class Booking extends Eloquent {
 	protected $table = 'Bookings';
+	protected $fillable = array('destination_branch_id', 'user_ids', 'start_date', 'end_date', 'kit_id');
 
 	public function item() {
 		return $this->belongsTo('Kit');
-	}	
-	
+	}   
 }
