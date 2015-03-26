3a829b58492855da6f2d17ae53b72b2eff153fc5
brookwellreuber <chaoticpsychosisss@gmail.com>
2015-03-26 15:21:37 -0400

    Booking Backend Logic
    Backend connects to DB, Mailing is still a work in progress

--- laravel/app/controllers/BookingsController.php
+++ laravel/app/controllers/BookingsController.php
@@ -2,89 +2,66 @@
 
 class BookingsController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-     */
-
-
-	public function index()
-    {
-        $data = array ( 'title' => "Create Booking" );
+	public function index() {
+		$data = array ( 'title' => "Create Booking" );
 		return View::make ('booking')->with($data);
 	}
 
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-        $data = array ( 'title' => "Create Booking" );
-        return View::make ('booking')->with($data);
+	public function create() {
+		$data = array ( 'title' => "Create Booking" );
+		return View::make ('booking')->with($data);
 	}
 
+	public function store() {
+		$startDate = Input::get('startDate');
+		$endDate = Input::get('endDate');
+		$kit = Input::get('kit');
+		$users = Input::get('users');
+		$users = explode (',', $users);
+		//$email = DB::table('users')->where('id' , '=' , "$users")->select('email')->get(); -- WIP line retained to work on in the next sprint.
+		array_shift($users);
+		array_pop($users);
+		array_push($users, Auth::User()->id);
+		$destBranch = Auth::User()->branch_id;
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
+
+		Booking::create(array(
+			'destination_branch_id' => $destBranch,
+			'user_ids' => serialize($users),
+			'start_date' => $startDate,
+			'end_date' => $endDate,
+			'kit_id' => $kit,
+		));
+
+
+		$email = "david.brookwell@shaw.ca";
+		$mail =  explode( ',' , $email );
+
+
+		Mail::send('emails.bookingEmail',[],function($message) use ($mail){
+
+			foreach ($mail as $recipient){
+
+				$message->to($recipient, 'Jdoe')->subject('Booking Creation');
+			}
+
+		});
+		return Redirect::to ('/summary');
+	}
+
+	public function show($id) {
 		//
 	}
 
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
+	public function edit($id) {
 		//
 	}
 
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
+	public function update($id) {
 		//
 	}
 
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
+	public function destroy($id) {
 		//
 	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
-
 }
---
+++ laravel/app/views/emails/bookingEmail.blade.php
@@ -0,0 +1,13 @@
+<h1>  Booking success! </h1>
+
+<p> Hello, David!
+
+	 Your booking as been successfully created.
+
+More information regarding the booking can be found in the KIT booking system, by clicking on the summary button.
+
+cheers,
+
+The Kit Management system.
+
+</p>
