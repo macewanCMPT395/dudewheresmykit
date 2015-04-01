62b31e15af5dd828be8cfe6d880ad123469e6c8b
Alexander Martin <martina52@mymacewan.ca>
2015-04-01 11:01:40 -0600

    Initial Implementation of Transfers

--- laravel/app/database/migrations/2015_03_10_100107_create_user_table.php
+++ laravel/app/database/migrations/2015_03_10_100107_create_user_table.php
@@ -13,16 +13,16 @@ class CreateUserTable extends Migration {
 	 */
 	public function up() {
 		Schema::create('Users', function(Blueprint $table) {
-            $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone');
-            //0=User, 1=Manager, 2=Admin
-            $table->integer('permission_id');
-            $table->rememberToken();
-            $table->integer('branch_id')->nullable();
+			$table->increments('id');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->string('phone');
+			//0=User, 1=Manager, 2=Admin
+			$table->integer('permission_id');
+			$table->rememberToken();
+			$table->integer('branch_id');
 			$table->timestamps();
 		});
 	}
