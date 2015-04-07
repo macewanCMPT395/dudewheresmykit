0696d2e076345b9bf7a939ca098492083e5f7eae
magaw <magaw@mymacewan.ca>
2015-04-06 22:14:59 -0600

    Database Seeding Upgrade
    Now no longer contains Lorem Ipsum data.
    Each kit contains 5 Items of kit type, and 5 chargers for kit type.

--- laravel/app/database/seeds/BookingTableSeeder.php
+++ laravel/app/database/seeds/BookingTableSeeder.php
@@ -8,29 +8,32 @@ class BookingTableSeeder extends Seeder {
 		$maxStatus = count(Status::all());
 
 		for($i = 0; $i < 50; ++$i){
-			$startDay = $faker->dateTimeBetween($startDate = 'now', $endDate = '+30 years');
-			$endDay = $faker->dateTimeBetween($startDate = $startDay, $endDate = '+31 years');
+			$startDay = $faker->dateTimeBetween($startDate = 'now', $endDate = '+6 months');
+			$endDay = $faker->dateTimeBetween($startDate = $startDay, $endDate = '+1 years');
 			$status_id = $faker->numberBetween(1, $maxStatus);
 			if ($status_id != 3) {
 				$shipped = Carbon\Carbon::now();
 			} else {
 				$shipped = '';
 			}
+			$kit = Kit::find($faker->numberBetween(1, $maxKit));
+			$event = $kit->type->name;
+			$event = substr($event, 0, strlen($event) - 1) . ' Workshop';
 			$booking = Booking::create(array(
 				'destination_branch_id' => $faker->numberBetween(1, $maxBranch),
-				'kit_id' => $faker->numberBetween(1, $maxKit),
+				'kit_id' => $kit->id,
 				'status_id' => $status_id,
 				'shipped' => $shipped,
 				'start_date' => $startDay,
 				'end_date' => $endDay,
-				'event' => $faker->word
+				'event' => $event
 			));
 
 			// Get random user ids to attach.
 			$users = array();
 			for ($j = 0; $j < 4; $j++) {
 				$number = $faker->numberBetween(1, $maxUser);
-				if (!in_array($number, $users)) { 
+				if (!in_array($number, $users)) {
 					array_push($users, $number);
 				}
 			}
--- laravel/app/database/seeds/ItemTableSeeder.php
+++ laravel/app/database/seeds/ItemTableSeeder.php
@@ -2,32 +2,27 @@
 class ItemTableSeeder extends Seeder {
 	public function run() {
 		$faker = Faker\Factory::create();
-		//Gen types of items
-		$names = array();
-		$descs = array();
-		$count = 5;
-		$maxKit = count(Kit::all());
-		for($i = $count; $i > -1; --$i){
-			array_push($names, $faker->word);
-			array_push($descs, $faker->text(64));
-		}
 
-		for($i = 0; $i < 200; ++$i){
-			$index = $faker->numberBetween(0, $count - 1);
-			if($faker->numberBetween(0, 10) == 5){
+		foreach(Kit::all() as $kit){
+			$type = $kit->type->name;
+			$type = substr($type, 0, strlen($type) - 1);
+			for($i = 0; $i < 5; ++$i){
 				Item::create(array(
-					'name' => $names[$index],
-					'description' => $descs[$index],
-					'kit_id' => $faker->numberBetween(1,$maxKit),
-					'asset_tag' => '31221' . $faker->unique()->numberBetween(100000, 999999),
-					'note' => $faker->text(64)
+					'name' => $type,
+					'description' => "Information about this $type",
+					'kit_id' => $kit->id,
+					'asset_tag' => $faker->unique()->numberBetween(100000, 999999),
+					'note' => ($faker->numberBetween(0, 20) == 5) ? (($faker->numberBetween(0, 20) == 5) ? "Badly damaged" : "Slightly damaged") : '',
 				));
-			}else{
+			}
+			$type .= ' Power Cable';
+			for($i = 0; $i < 5; ++$i){
 				Item::create(array(
-					'name' => $names[$index],
-					'description' => $descs[$index],
-					'kit_id' => $faker->numberBetween(1,$maxKit),
-					'asset_tag' => '31221' . $faker->unique()->numberBetween(100000, 999999)
+					'name' => $type,
+					'description' => "Information about this $type",
+					'kit_id' => $kit->id,
+					'asset_tag' => $faker->unique()->numberBetween(100000, 999999),
+					'note' => ($faker->numberBetween(0, 20) == 5) ? (($faker->numberBetween(0, 20) == 5) ? "Badly damaged" : "Slightly damaged") : '',
 				));
 			}
 		}
