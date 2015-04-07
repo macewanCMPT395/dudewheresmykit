bc838a91f963a093ff7ee42cf9b6c8baf5126c58
Alexander Martin <martina52@mymacewan.ca>
2015-04-06 23:21:06 -0600

    Implemented CRUD Operations for Kit Types

--- laravel/app/models/KitType.php
+++ laravel/app/models/KitType.php
@@ -6,4 +6,13 @@ class KitType extends Eloquent {
 	public function kits() {
 		return $this->hasMany('Kit', 'type_id');
 	}
+
+	public static function validate($input, $edit=false) {
+		$rules = array(
+			'name' => "Required" . ($edit ? '' : '|Unique:KitTypes')
+		);
+
+		$validator = Validator::make($input,$rules);
+		return $validator;
+	}	
 }
