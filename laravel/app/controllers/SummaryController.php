cce0c87cf5bc6d1a1ea84a24ee44e2826f97ad6e
leehumeniuk <lee.humeniuk@gmail.com>
2015-04-08 12:54:25 -0400

    Format Bookings Table
    Implement Drop Down Filter
    
    Implement Delete Button

--- laravel/app/controllers/SummaryController.php
+++ laravel/app/controllers/SummaryController.php
@@ -8,12 +8,29 @@ class SummaryController extends \BaseController {
 	 * @return Response
 	 */
 	public function index() {
-		$user = Auth::user();
-		$results = $user->currentBookings;
-		$data = array (
-			'title' => "Summary",
-			'results' => $results
-		);
-		return View::make('summary')->with($data);
+		$branches = DB::table('Branches')
+			->select('id', 'name', 'code')
+			->orderBy('name', 'asc')->get();
+		$filter_value = Input::get('branch');
+		if($filter_value != 'mybookings' && $filter_value != '') {
+			$data = array (
+				'title' => "Bookings",
+				'bookings' => Booking::whereRaw('start_date >= date() AND destination_branch_id = '.$filter_value)->get(),
+				'branches' => $branches,
+				'curBranch' => $filter_value
+			);
+			return View::make('summary')->with($data);
+		}else{
+			$data = array (
+				'title' => "Bookings",
+				'bookings' => Booking::whereHas('users', function($q) {
+						$q->where('user_id', '=', Auth::user()->id);
+					})
+						->whereRaw('start_date >= date()')->get(),
+							'branches' => $branches,
+							'curBranch' => $filter_value
+			);
+			return View::make('summary')->with($data);
+		}
 	}
 }
