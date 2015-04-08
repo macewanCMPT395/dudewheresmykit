b7d4684710343af9e227eb8bb6fdfbf4ce97bff9
Alexander Martin <martina52@mymacewan.ca>
2015-04-08 12:14:48 -0600

    Home Page and Seeder Fix

--- laravel/app/controllers/HomeController.php
+++ laravel/app/controllers/HomeController.php
@@ -2,22 +2,18 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+	public function home() {
+		$newKitTypes = KitType::orderBy('created_at', 'desc')->take(2)->get();
+		$branch = Auth::user()->branch;
+		$incomingBookings = Booking::whereRaw("status_id = 1 AND destination_branch_id = " . $branch->id)->get();
+		$outgoingBookings = $branch->activeOutgoingBookings(true);
+		$data = array(
+			'newKitTypes' => $newKitTypes,
+			'title' => 'Home',
+			'incomingBookings' => $incomingBookings,
+			'outgoingBookings' => $outgoingBookings
+		);
 
-	public function showWelcome()
-	{
-		return View::make('hello');
+		return View::make('home')->with($data);
 	}
-
 }
