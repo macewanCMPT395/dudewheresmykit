05ec2abd3cb4a5e60828aea8c0d5486495bf6f42
Alexander Martin <martina52@mymacewan.ca>
2015-04-09 15:10:02 -0600

    Updated Home Page

--- laravel/app/views/home.blade.php
+++ laravel/app/views/home.blade.php
@@ -1,52 +1,59 @@
 @extends ('master')
 @section ('content')
-	<h2>Current Incoming Bookings</h2>
-	<table>
-		<tr>
-			<th style="width:100px;">Event</th>
-			<th style="width:150px;">Start Date</th>
-			<th style="width:150px;">End Date</th>
-			<th style="width:100px;">Status</th>
-		</tr>
-		@foreach($incomingBookings as $booking) 
-			<tr>
-				<td>{{{ $booking->event }}}</td>
-				<td>{{{ $booking->start_date }}}</td>
-				<td>{{{ $booking->end_date }}}</td>
-				<td>{{{ $booking->status->description }}}</td>
-			</tr>
-		@endforeach	
-	</table>
+	<h2>Current Incoming Transfers</h2>
 
-	<h2>Awaiting Transfer</h2>
-	<table>
-		<tr>	
-			<th style="width:100px;">Event</th>
-			<th style="width:150px;">Start Date</th>
-			<th style="width:150px;">End Date</th>
-			<th style="width:100px;">Status</th>
-		</tr>
-		@foreach($outgoingBookings as $booking) 
+	<a href="{{{ url("/transfers") }}}">
+		<table>
 			<tr>
-				<td>{{{ $booking->event }}}</td>
-				<td>{{{ $booking->start_date }}}</td>
-				<td>{{{ $booking->end_date }}}</td>
-				<td>{{{ $booking->status->description }}}</td>
+				<th style="width:100px;">Event</th>
+				<th style="width:100px;">Kit Type</th>
+				<th style="width:150px;">Barcode</th>
+				<th style="width:150px;">Event Start Date</th>
+				<th style="width:100px;">Status</th>
 			</tr>
-		@endforeach	
-	</table>
-		
+			@foreach($incomingBookings as $booking)
+				<tr>
+					<td>{{{ $booking->event }}}</td>
+					<td>{{{ $booking->kit->type->name }}}</td>
+					<td>{{{ $booking->kit->code }}}</td>
+					<td>{{{ $booking->start_date }}}</td>
+					<td>{{{ $booking->status->description }}}</td>
+				</tr>
+			@endforeach
+		</table>
+	</a>
+
+	<h2>Current Outgoing Transfers</h2>
+	<a href="{{{ url("/transfers") }}}">
+		<table>
+			<tr>
+				<th style="width:100px;">Kit Type</th>
+				<th style="width:150px;">Barcode</th>
+				<th style="width:150px;">Destination</th>
+				<th style="width:100px;">Status</th>
+			</tr>
+			@foreach($outgoingBookings as $booking)
+				<tr>
+					<td>{{{ $booking->kit->type->name }}}</td>
+					<td>{{{ $booking->kit->code }}}</td>
+					<td>{{{ $booking->kit->branch->name }}} ({{{ $booking->kit->branch->code }}})</td>
+					<td>{{{ $booking->status->description }}}</td>
+				</tr>
+			@endforeach
+		</table>
+	</a>
+
 	<h2>New Kit Types</h2>
 	<table>
 		<tr>
 			<th>Kit Type</th>
 			<th>Amount of Kits</th>
 		</tr>
-		@foreach($newKitTypes as $type) 
+		@foreach($newKitTypes as $type)
 			<tr>
 				<td>{{{ $type->name }}}</td>
 				<td>{{{ count($type->kits) }}}</td>
 			</tr>
-		@endforeach	
+		@endforeach
 	</table>
 @stop
