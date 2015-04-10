da6e3f9df5503eecc714ae6c9f27e9c1cc526627
magaw <magaw@mymacewan.ca>
2015-04-09 23:34:29 -0600

    Home Page Improvement
    Fixes misc issues with the home page such as:
    Whole table being highlighted when one row is selected and
    A kit being displayed multiple times because it has multiple bookings.

--- laravel/app/controllers/HomeController.php
+++ laravel/app/controllers/HomeController.php
@@ -6,7 +6,13 @@ class HomeController extends BaseController {
 		$newKitTypes = KitType::orderBy('created_at', 'desc')->take(2)->get();
 		$branch = Auth::user()->branch;
 		$incomingBookings = Booking::whereRaw("status_id = 1 AND destination_branch_id = " . $branch->id)->get();
-		$outgoingBookings = $branch->activeOutgoingBookings(true);
+		$outgoingBookings = array();
+		$unique_kits = array();
+		foreach($branch->activeOutgoingBookings(true) as $booking){
+			if(in_array($booking->kit_id, $unique_kits)) continue;
+			array_push($outgoingBookings, $booking);
+			array_push($unique_kits, $booking->kit_id);
+		}
 		$data = array(
 			'newKitTypes' => $newKitTypes,
 			'title' => 'Home',
--- laravel/app/views/home.blade.php
+++ laravel/app/views/home.blade.php
@@ -1,47 +1,42 @@
 @extends ('master')
 @section ('content')
 	<h2>Current Incoming Transfers</h2>
-
-	<a href="{{{ url("/transfers") }}}">
-		<table>
-			<tr>
-				<th style="width:100px;">Event</th>
-				<th style="width:100px;">Kit Type</th>
-				<th style="width:150px;">Barcode</th>
-				<th style="width:150px;">Event Start Date</th>
-				<th style="width:100px;">Status</th>
+	<table>
+		<tr>
+			<th style="width:100px;">Event</th>
+			<th style="width:100px;">Kit Type</th>
+			<th style="width:150px;">Barcode</th>
+			<th style="width:150px;">Event Start Date</th>
+			<th style="width:100px;">Status</th>
+		</tr>
+		@foreach($incomingBookings as $booking)
+			<tr onclick="window.location='{{url("/transfers")}}'">
+				<td>{{{ $booking->event }}}</td>
+				<td>{{{ $booking->kit->type->name }}}</td>
+				<td>{{{ $booking->kit->code }}}</td>
+				<td>{{{ $booking->start_date }}}</td>
+				<td>{{{ $booking->status->description }}}</td>
 			</tr>
-			@foreach($incomingBookings as $booking)
-				<tr>
-					<td>{{{ $booking->event }}}</td>
-					<td>{{{ $booking->kit->type->name }}}</td>
-					<td>{{{ $booking->kit->code }}}</td>
-					<td>{{{ $booking->start_date }}}</td>
-					<td>{{{ $booking->status->description }}}</td>
-				</tr>
-			@endforeach
-		</table>
-	</a>
+		@endforeach
+	</table>
 
 	<h2>Current Outgoing Transfers</h2>
-	<a href="{{{ url("/transfers") }}}">
-		<table>
-			<tr>
-				<th style="width:100px;">Kit Type</th>
-				<th style="width:150px;">Barcode</th>
-				<th style="width:150px;">Destination</th>
-				<th style="width:100px;">Status</th>
+	<table>
+		<tr>
+			<th style="width:100px;">Kit Type</th>
+			<th style="width:150px;">Barcode</th>
+			<th style="width:150px;">Destination</th>
+			<th style="width:100px;">Status</th>
+		</tr>
+		@foreach($outgoingBookings as $booking)
+			<tr onclick="window.location='{{url("/transfers")}}'">
+				<td>{{{ $booking->kit->type->name }}}</td>
+				<td>{{{ $booking->kit->code }}}</td>
+				<td>{{{ $booking->kit->branch->name }}} ({{{ $booking->kit->branch->code }}})</td>
+				<td>{{{ $booking->status->description }}}</td>
 			</tr>
-			@foreach($outgoingBookings as $booking)
-				<tr>
-					<td>{{{ $booking->kit->type->name }}}</td>
-					<td>{{{ $booking->kit->code }}}</td>
-					<td>{{{ $booking->kit->branch->name }}} ({{{ $booking->kit->branch->code }}})</td>
-					<td>{{{ $booking->status->description }}}</td>
-				</tr>
-			@endforeach
-		</table>
-	</a>
+		@endforeach
+	</table>
 
 	<h2>New Kit Types</h2>
 	<table>
